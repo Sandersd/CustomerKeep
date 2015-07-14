55d7e4ed0634b4cccf0cf22fe12342555853e102
Dylan Sanders <sandersd27@gmail.com>
2015-07-13 22:23:56 -0700

    separated functionality into services

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -9,17 +9,40 @@ angular.module('myApp.view1', ['ngRoute'])
   });
 }])
 
-.controller('View1Ctrl', ['$scope', '$location', function($scope, $location) {
+.controller('View1Ctrl', ['$scope', 'AuthenticationService', function($scope, AuthenticationService) {
   $scope.creds = {username: "", password: ""};
   $scope.login = function() {
-    if($scope.creds.username === 'admin' && $scope.creds.password === '1234') {
-      $location.path('/dashboard');
-    } else {
-      $('.alert-box').show();
-    }
-  },
-
-  $scope.logout = function() {
-    $location.path('/');
+    AuthenticationService.login($scope.creds);
   }
-}]);
+}])
+.factory("AuthenticationService", ['$location', function($location) {
+    return {
+      login : function(creds) {
+      if(creds.username == 'admin' && creds.password == '1234') {
+        $location.path('/dashboard');
+      } else {
+        $('.alert-box').show();
+      }
+    }
+
+    /*$scope.logout = function() {
+      $location.path('/');
+    }*/
+  }
+}])
+  .factory("CustomerService", [function(){
+    var customers = [
+    {name: 'John Doe', address: '155 Main St', city: 'Boston MA'},
+    {name: 'Steve Smith', address: '235 Mass Ave', city: 'Cambridge MA'},
+    {name: 'Carol Stevens', address: '12 Congress St', city: 'Ashbury MA'},
+    {name: 'Kyle Savon', address: '111 School St', city: 'Anne MA'},
+    {name: 'Ben Button', address: '122 Union St', city: 'Lynn MA'},
+    {name: 'Derek Salmon', address: '15 Marsston St', city: 'Lawrence MA'}
+  ];
+    var factory= {};
+    factory.getCustomers = function() {
+      return customers;
+    }
+
+    return factory;
+  }]);
--- app/view2/view2.js
+++ app/view2/view2.js
@@ -13,13 +13,6 @@ angular.module('myApp.view2', ['ngRoute'])
   $scope.title = "Dashboard";
 }])
 
-.controller('CustomerCtrl', ['$scope', function($scope) {
-  $scope.customers = [
-    {name: 'John Doe', address: '155 Main St', city: 'Boston MA'},
-    {name: 'Steve Smith', address: '235 Mass Ave', city: 'Cambridge MA'},
-    {name: 'Carol Stevens', address: '12 Congress St', city: 'Ashbury MA'},
-    {name: 'Kyle Savon', address: '111 School St', city: 'Anne MA'},
-    {name: 'Ben Button', address: '122 Union St', city: 'Lynn MA'},
-    {name: 'Derek Salmon', address: '15 Marsston St', city: 'Lawrence MA'}
-  ];
+.controller('CustomerCtrl', ['$scope', 'CustomerService', function($scope, CustomerService) {
+  $scope.customers = CustomerService.getCustomers();
 }]);
